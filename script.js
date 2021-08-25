ab19b521c3bf0ec020bf30e2a8f3e6a4049be402
acidicNic <nic4096@gmail.com>
2021-08-25 14:37:08 -0700

    Challenge 3: Completed

---
+++ script.js
@@ -0,0 +1,23 @@
+const box = document.getElementById("custom-box");
+
+const width = document.getElementById("width");
+const height = document.getElementById("height");
+const color = document.getElementById("color");
+
+const widthText = document.getElementById("width-text");
+const heightText = document.getElementById("height-text");
+const colorText = document.getElementById("color-text");
+
+height.addEventListener("input", updateBox);
+width.addEventListener("input", updateBox);
+color.addEventListener("input", updateBox);
+
+
+function updateBox() {
+  box.style.height = `${height.value}px`;
+  box.style.width = `${width.value}px`;
+  box.style.backgroundColor = color.value;
+  heightText.textContent = height.value;
+  widthText.textContent = width.value;
+  colorText.textContent = color.value;
+}
